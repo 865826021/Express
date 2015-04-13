fef7829c5194d7aa297d2b6afd3455eab4d99710
yicheng <yicheng@juanpi.com>
2015-04-13 12:01:39 +0800

    修改demo

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type: text/html; charset=utf-8");
 	include 'Express.php';
 	echo '<pre>';
-	var_dump(json_decode(Express::getExpressInfo('868124032474'),true));
+	var_dump(json_decode(Express::getExpressInfo('210793228260'),true));
