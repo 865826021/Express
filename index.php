410f039452522e3d19ae4cee9ad193745cae06b9
justmd5 <justmd5@163.com>
2015-06-02 16:15:11 +0800

    Update index.php
    修改示例查询订单号

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 header("Content-type: text/html; charset=utf-8");
 	include 'Express.php';
 	echo '<pre>';
-	var_dump(json_decode(Express::getExpressInfo('210793228260'),true));
+	var_dump(json_decode(Express::getExpressInfo('7571742361111'),true));
