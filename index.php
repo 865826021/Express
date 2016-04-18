b9c812ff81efb520c818e053afdf03f08fa19500
justmd5 <ding21st@gmail.com>
2016-04-18 17:56:39 +0800

    加入phpunit单元测试

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 use JustMd5\Express\Express;
 
 require 'vendor/autoload.php';
-//echo var_export(Express::getExpressInfo(881443775034378914), true), PHP_EOL;
-//echo var_export(Express::getExpressName(881443775034378914), true), PHP_EOL;
-print_r(Express::getExpressInfo(881443775034378914));
+echo var_export(Express::getExpressInfo(881443775034378914), true), PHP_EOL;
 
